edc7c89868c7c690b2a98210ebb46fbc13b71dea
Chand Bud <chandbud5@gmail.com>
2020-07-03 17:16:09 +0530

    Add files via upload

---
+++ math.py
@@ -0,0 +1,38 @@
+Python 3.8.0 (tags/v3.8.0:fa919fd, Oct 14 2019, 19:37:50) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> import math
+>>> math.pow(3,6)
+729.0
+>>> # SQuare root
+>>> math.sqrt(25)
+5.0
+>>> math.sqrt(3.6)
+1.8973665961010275
+>>> math.e
+2.718281828459045
+>>> math.pi
+3.141592653589793
+>>> math.ceil(5.7)
+6
+>>> math.ceil(5.1)
+6
+>>> math.floor(5.9)
+5
+>>> math.floor(5.1)
+5
+>>> math.factorial(7)
+5040
+>>> import math as m
+>>> m.factorial(6)
+720
+>>> math.factorial(8)
+40320
+>>> from math import pow, sqrt
+>>> pow(3,4)
+81.0
+>>> sqrt(8)
+2.8284271247461903
+>>> from math import *
+>>> floor(6.8)
+6
+>>> 
